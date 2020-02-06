0ee35d67b05bd38613aed307465ad2aa21c078cb
VU QUANG NAM <vuquangnam1507@gmail.com>
2020-02-06 09:08:29 +0700

    add code role

---
+++ middleware/rolemiddleware.js
@@ -0,0 +1,15 @@
+module.exports = authorize;
+
+function authorize(roles = []) {
+    if (typeof roles === 'string') {
+        roles = [roles];
+    }
+    return [
+        (req, res, next) => {
+            if (roles.length && !roles.includes(req.userData.role)) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+            next();
+        }
+    ];
+}
--- user/routes/api-routes.js
+++ user/routes/api-routes.js
@@ -1,9 +1,8 @@
 let router = require('express').Router();
-const checkAuth = require('./AuthMiddleware')
+const checkAuth = require('../../middleware/AuthMiddleware')
 const { Validation } = require('../validate/validate')
 const validate = require('express-validation')
-
-const checkRole = require('../role/role')
+const authorize = require('../../middleware/rolemiddleware')
 
 // Set default API response
 router.get('/', function (req, res) {
@@ -17,8 +16,8 @@ var userController = require('../controller/user.controller');
 
 // user routes
 router.route('/users')
-    .get(checkAuth, checkRole, userController.list)
-    .post(checkAuth, validate(Validation), userController.create);
+    .get(checkAuth, authorize('admin'), userController.list)
+    .post(checkAuth, authorize('admin'), validate(Validation), userController.create);
 router.route('/users/:user_id')
     .get(checkAuth, userController.detail)
     .put(checkAuth, validate(Validation), userController.update)
