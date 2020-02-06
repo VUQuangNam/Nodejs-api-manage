db1e9ef35ecfca926e6d5e673d44bcce37351fcc
VU QUANG NAM <vuquangnam1507@gmail.com>
2020-02-06 18:03:47 +0700

    push code

--- user/middleware/rolemiddleware.js
+++ user/middleware/rolemiddleware.js
@@ -1,13 +1,16 @@
 module.exports = authorize;
-
+const User = require('../model/user.model')
 function authorize(roles) {
     return [
         (req, res, next) => {
-            const check = req.userData.role.find(x => x === roles);
-            if (!check) {
-                return res.status(401).json({ message: 'Bạn không có đủ quyền để thực hiện hành động này' });
-            }
-            next();
+            User.findById(req.userData.id, function (error, user) {
+                if (error) return res.json({ message: 'Có lỗi xảy ra' });
+                if (!user) return res.json({ message: 'Tài khoản không tồn tại' });
+                const check = user.role.find(x => x === roles);
+                if (!check) return res.status(401).json({ message: 'Bạn không có đủ quyền để thực hiện hành động này' });
+                next();
+            });
+
         }
     ];
 }
